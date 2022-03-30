5bc5e27a66329fb94e830ddabc2e84968ea6c6c9
xfirdavs <firdavsjuraboyevuz1@gmail.com>
2022-03-30 06:00:13 -0400

    bash file modified

--- mycommands.sh
+++ mycommands.sh
@@ -12,4 +12,8 @@ echo ".......list of services......"
 systemctl --type=service --state=running
 echo ".......active procces......."
 ps aux
+echo ".......cpu info........"
+cat /proc/cpuinfo
+echo ".......ram info........"
+cat /proc/meminfo
 echo ".......Thanks a lot......."
